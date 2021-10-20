84a84cae35cf2ccd0f11f1259bbb58e1936bf0fc
cshigeoka <shigeoka@usc.edu>
2021-10-19 19:12:00 -0700

    trail changes

--- Assets/Scripts/TrailController.cs
+++ Assets/Scripts/TrailController.cs
@@ -9,12 +9,11 @@ public class TrailController : MonoBehaviour
     public TrailRenderer trail; //the trail
     //public GameObject TrailFollower;
     public GameObject ColliderPrefab;
-    public float followDistance = 1f;
+    
 
     public int poolSize = 5;
     GameObject[] pool;
     float trailRenderTime;
-    GameObject player;
 
     void Start()
     {
@@ -29,11 +28,6 @@ public class TrailController : MonoBehaviour
 
     void Update()
     {
-        if (!player)
-        {
-            player = GameObject.FindGameObjectWithTag("Player");
-        }
-
         if (!trail.isVisible)
         {
             for (int i = 0; i < pool.Length; i++)
@@ -47,13 +41,6 @@ public class TrailController : MonoBehaviour
             TrailCollission();
         }
 
-    }
-
-    void LateUpdate()
-    {
-        transform.position = player.transform.position - player.transform.forward * followDistance;
-        transform.LookAt(player.transform.position);
-        transform.position = new Vector3(transform.position.x, transform.position.y + 5, transform.position.z);
     }
 
     void TrailCollission()
